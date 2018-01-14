3862f73450d35a799812be9f89bbdca71a24417c
Artur Grabowski <art@blahonga.org>
2018-01-14 16:48:21 +0100

    First relatively functioning version.

---
+++ cp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+D='/c/users/art/x3/luMayhem - FDN/addon/scripts/'
+
+cp "$D/setup.Unilog.xml" .
+cp "$D/Unilog.Menu.xml" .
+cp "$D/Unilog.Menu.SM.xml" .
+cp "$D/Unilog.Monitor.Dock.xml" .
+cp "$D/Unilog.Cmd.Freighter.xml" .
