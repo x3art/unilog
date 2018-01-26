b2422a433f8bc86b521bb82e91d1f34876336421
Artur Grabowski <art@blahonga.org>
2018-01-26 10:08:24 +0100

    Move stuff around.

--- cp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-D='/c/users/art/x3/luMayhem - FDN/addon/scripts/'
-
-cp "$D/setup.Unilog.xml" .
-cp "$D/Unilog.Menu.xml" .
-cp "$D/Unilog.Menu.SM.xml" .
-cp "$D/Unilog.Monitor.Dock.xml" .
-cp "$D/Unilog.Cmd.Freighter.xml" .
